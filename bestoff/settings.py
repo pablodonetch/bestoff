efba7d966741de4ef6fe9c5b601900a049f033ac
Pablo Donetch <pablodonetch@gmail.com>
2022-09-30 17:51:00 -0300

    navbar

--- bestoff/settings.py
+++ bestoff/settings.py
@@ -18,12 +18,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = [
-    '127.0.0.1'
-    'bestoff.herokuapp.com'
-]
+#ALLOWED_HOSTS = [
+#    '127.0.0.1'
+#    'bestoff.herokuapp.com'
+#]
 
-#ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -124,7 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
