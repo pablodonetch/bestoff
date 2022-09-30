a5b296613e69069af45c8da85da7c16e5ed8755f
Pablo Donetch <pablodonetch@gmail.com>
2022-09-30 16:21:06 -0300

    tailwind

--- bestoff/settings.py
+++ bestoff/settings.py
@@ -23,6 +23,7 @@ ALLOWED_HOSTS = [
     'bestoff.herokuapp.com'
 ]
 
+#ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -33,7 +34,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+
+    'tailwind',
+    'theme',
 ]
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
