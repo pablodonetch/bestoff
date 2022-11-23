5c882748297aba0cfa80b6f05c4b248af583fbec
pablodonetch <pablodonetch@gmail.com>
2022-11-23 20:34:46 -0300

    20221123_08

--- bestoff/views.py
+++ bestoff/views.py
@@ -10,9 +10,17 @@ class HomeView(View):
     def get(self, request, *args, **kwargs):
         propiedades = Propiedad.objects.all()
         images = Image.objects.all()
+        UF=34500 #CAMBIAR UF ACÁ
+        rentabilidades=[]
+        rentabilidades.append({})
+        for propiedad in propiedades:
+            rentabilidad_real= float(f'{((propiedad.arriendo_actual*12)/(propiedad.precio*UF)*100):.1f}')
+            rentabilidades[0][propiedad.id]=rentabilidad_real
+        print(rentabilidades)
         context={ 
             'propiedades': propiedades,
             'images': images,
+            'rentabilidades': rentabilidades,
         }
         return render(request, 'pages/index.html', context)
 
