7ac1c3d22dcfe26d4883e2a3730cec2491b2a05d
Pablo Donetch <pablodonetch@gmail.com>
2023-08-10 12:13:40 -0400

    20230810_02

--- acceso/forms.py
+++ acceso/forms.py
@@ -5,5 +5,5 @@ from django.forms import ModelForm, PasswordInput
 
 
 class Formulario_Login(forms.Form):
-    correo = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class': '', 'placeholder': 'E-Mail', 'autocomplete':'off'}))
-    password = forms.CharField(widget=PasswordInput(attrs={'class': '', 'placeholder': 'Contraseña', 'autocomplete':'off'}))
+    correo = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class': 'rounded-md focus:border-orange-500', 'placeholder': 'E-Mail', 'autocomplete':'off'}))
+    password = forms.CharField(widget=PasswordInput(attrs={'class': 'rounded-md focus:border-orange-500 ', 'placeholder': 'Contraseña', 'autocomplete':'off'}))
--- bestoff/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, re_path
-from bestoff.consumers import WSConsumer
-
-websocket_urlpatterns = [
-	re_path('ws/propiedad/(?P<propiedad_id>\d+)', WSConsumer.as_asgi()),
-]
