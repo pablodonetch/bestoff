be9a12a7de708272be37640d0bd74a812e615919
Pablo Donetch <pablodonetch@gmail.com>
2023-08-11 12:46:01 -0400

    20230811_01

--- acceso/forms.py
+++ acceso/forms.py
@@ -5,5 +5,5 @@ from django.forms import ModelForm, PasswordInput
 
 
 class Formulario_Login(forms.Form):
-    correo = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class': 'rounded-md focus:border-orange-500', 'placeholder': 'E-Mail', 'autocomplete':'off'}))
-    password = forms.CharField(widget=PasswordInput(attrs={'class': 'rounded-md focus:border-orange-500 ', 'placeholder': 'Contraseña', 'autocomplete':'off'}))
+    correo = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class': 'w-full rounded-md mx-0 border-2 border-orange-200 py-4 px-2', 'placeholder': 'E-Mail', 'autocomplete':'off'}))
+    password = forms.CharField(widget=PasswordInput(attrs={'class': 'w-full rounded-md mx-0 border-2 border-orange-200 py-4 px-2', 'placeholder': 'Contraseña', 'autocomplete':'off'}))
